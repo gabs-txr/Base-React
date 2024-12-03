2099f3977c3369afedf82fbcef25926fb8c655a4
gabs-txr <127438400+gabs-txr@users.noreply.github.com>
2024-12-03 09:58:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Button from './components/button/button';
 function App() {
   return (
     <>
-      <ColoredText text ='Esse texto ficará maiúsico nao importa como foi escrtio' color = 'red'/>
+      <ColoredText text ='Esse texto ficará maiúsculo nao importa como foi escrtio' color = 'red'/>
       <ColoredText />
 
       <Button label='insira a label aqui via JS'/>
